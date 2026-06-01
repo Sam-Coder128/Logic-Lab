91a67583fbc7cdd961eb0289cd87a5ccc835ceb6
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-01 17:58:38 +0530

    Add rectangle area calculation program in C
    This program calculates the area of a rectangle based on user input for width and height, demonstrating the use of functions in C.

---
+++ C/Ass8q2.c
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Program      : Rectangle Area Calculation Demo
+// Input        : Float width and height entered by user
+// Output       : Prints area of rectangle
+// Methods      : RectArea(), printf(), scanf()
+// Description  : Demonstrates rectangle area calculation using formula width × height in C.
+// Author       : Samruddh Shivkumar Birajdar
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Input : 5.3 9.78
+// Output : 51.834
+
+#include<stdio.h>
+
+double RectArea(float fWidth, float fHeight)
+{
+    return fWidth * fHeight;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application : Calculates area of rectangle based on user-entered width and height.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    float fValue1 = 0.0, fValue2 = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter width:");
+    scanf("%f",&fValue1);
+
+    printf("Enter height:");
+    scanf("%f",&fValue2);
+
+    dRet = RectArea(fValue1, fValue2);
+
+    printf("Area of Rectangle is : %lf",dRet);
+
+    return 0;
+}
+
+/*
+Time Complexity : O(1)
+
+Explanation:
+No loop is used.
+Only width and height are processed once.
+*/
