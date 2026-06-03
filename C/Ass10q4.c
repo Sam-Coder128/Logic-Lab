8cfb2e8aac5edc4a7a15ee18b5e4dcaf00b60534
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-03 16:48:34 +0530

    Create Ass10q4.c

---
+++ C/Ass10q4.c
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Program      : Fahrenheit to Celsius Conversion Demo
+// Input        : Float temperature in Fahrenheit entered by user
+// Output       : Prints temperature in Celsius
+// Methods      : FhtoCs(), printf(), scanf()
+// Description  : Demonstrates conversion of Fahrenheit to Celsius in C.
+// Author       : Samruddh Shivkumar Birajdar
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Input : 10
+// Output : -12.2222
+// Input : 34
+// Output : 1.11111
+
+#include<stdio.h>
+
+double FhtoCs(float fTemp)
+{
+    return (fTemp - 32) * 5 / 9;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application : Converts temperature from Fahrenheit to Celsius.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter temperature in Fahrenheit:");
+    scanf("%f",&fValue);
+
+    dRet = FhtoCs(fValue);
+
+    printf("Temperature in Celsius is : %lf",dRet);
+
+    return 0;
+}
+
+/*
+Time Complexity : O(1)
+Explanation:
+No loop is present.
+The program performs fixed operations only.
+*/
