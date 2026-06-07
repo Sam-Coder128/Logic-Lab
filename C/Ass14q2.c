cc361f0a67a7053cb3c00182cfff0abf122aaf4a
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-07 17:39:13 +0530

    Implement frequency difference calculation for integers
    This program calculates the difference between the frequency of even and odd integers entered by the user. It includes memory allocation and error handling for user input.

---
+++ C/Ass14q2.c
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Program      : Frequency Difference Demo
+// Input        : Array of integers entered by user
+// Output       : Prints difference between frequency of even and odd elements
+// Methods      : Frequency(), printf(), scanf(), malloc(), free()
+// Description  : Demonstrates array traversal and frequency difference calculation in C.
+// Author       : Samruddh Shivkumar Birajdar
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Frequency(int Arr[], int iLength)
+{
+    int i = 0;
+    int iEven = 0;
+    int iOdd = 0;
+
+    for (i = 0; i < iLength; i++)
+    {
+        if ((Arr[i] % 2) == 0)
+        {
+            iEven++;
+        }
+        else
+        {
+            iOdd++;
+        }
+    }
+
+    return (iEven - iOdd);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application : Calculates difference between frequency of even and odd elements.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iSize = 0, iRet = 0, i = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements: ");
+    if (scanf("%d", &iSize) != 1 || iSize <= 0)
+    {
+        printf("Invalid size\n");
+        return 1;
+    }
+
+    p = (int *)malloc(iSize * sizeof(int));
+    if (p == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return 1;
+    }
+
+    printf("Enter %d elements:\n", iSize);
+    for (i = 0; i < iSize; i++)
+    {
+        if (scanf("%d", &p[i]) != 1)
+        {
+            printf("Invalid input\n");
+            free(p);
+            return 1;
+        }
+    }
+
+    iRet = Frequency(p, iSize);
+    printf("%d\n", iRet);
+
+    free(p);
+    return 0;
+}
