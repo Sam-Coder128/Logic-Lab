a97e948e24dc905c4384b1133b671ba308298ff5
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-13 16:04:46 +0530

    Add leap year check program
    This program checks if a given year is a leap year and demonstrates its functionality by checking the year 2024.

---
+++ Java/Ass19q1.py
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Program      : Leap Year Check Demo
+// Input        : Year (integer)
+// Output       : Whether the year is leap or not
+// Methods      : checkLeapYear(), System.out.println()
+// Description  : Checks if a given year is a leap year
+// Author       : Samruddh Shivkumar Birajdar
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Logic
+{
+    void checkLeapYear(int iYear)
+    {
+        if((iYear % 400 == 0) || ((iYear % 4 == 0) && (iYear % 100 != 0)))
+            System.out.println(iYear + " is a Leap Year");
+        else
+            System.out.println(iYear + " is not a Leap Year");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application : Demonstrates leap year check.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class A19q1
+{
+    public static void main(String[] args)
+    {
+        Logic lobj = new Logic();
+        lobj.checkLeapYear(2024);
+    }
+}
