02edefd00cd2c3f300017d987f04907cfed68f1d
Sam-Coder128 <samruddh.birajdar24@gmail.com>
2026-06-27 18:30:57 +0530

    Implement string reversal in place
    This program reverses a string in place using two pointers.

---
+++ C/A33q5.c
@@ -0,0 +1,58 @@
+// Input : “abcd”
+// Output : “dcba”
+//
+// Input : “abba”
+// Output : “abba”
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Program      : Reverse String In Place Demo
+// Description  : Reverses string in place
+// Author       : Samruddh Shivkumar Birajdar
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void StrRevX(char *str)
+{
+    char *start = str;
+    char *end = str;
+    char temp;
+
+    while(*end != '\0')
+    {
+        end++;
+    }
+    end--;
+
+    while(start < end)
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Application : Reverses string in place.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter string : ");
+    scanf("%[^'\n']s",arr);
+
+    StrRevX(arr);
+
+    printf("Modified string is %s",arr);
+
+    return 0;
+}
